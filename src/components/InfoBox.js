bcc020669e375cb73d9ba3d0685d5a0a0263fe35
matthewlstillings <matthewlstillings@gmail.com>
2019-11-21 21:24:46 -0500

    Infobox Update

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -1,21 +1,29 @@
-import React from 'react';
-import {InfoBox} from './styles';
+import React from "react";
+import { InfoBox } from "./styles";
 
-
-//Information box for extended information about fuel station including: Station name, address, access times. More data on station exists and can be added.  
-export default (props) => {
-    return (
-        <InfoBox className="infobox">
-            <p className="infobox_title">{props.info.station_name}</p>
-            <p className="infobox_hours">Open: {props.info.access_days_time}</p>
-            <p className="infobox_street">{props.info.street_address}</p>
-            <p className="infobox_address">
-              {props.info.city}, {props.info.state}, {props.info.zip}
-            </p>
-            <p className="close_info" onClick={()=>{
-              props.setInfo(null);
-            }}>Close</p>
-        </InfoBox>
-    )
-}
-
+//Information box for extended information about fuel station including: Station name, address, access times. More data on station exists and can be added.
+export default props => {
+  return (
+    <InfoBox className="infobox">
+      <p className="infobox_title">{props.info.station_name}</p>
+      <p className="infobox_hours">
+        Open -
+        {props.info.access_days_time
+          ? ` ${props.info.access_days_time}`
+          : " Unknown"}
+      </p>
+      <p className="infobox_street">{props.info.street_address}</p>
+      <p className="infobox_address">
+        {props.info.city}, {props.info.state}, {props.info.zip}
+      </p>
+      <p
+        className="close_info"
+        onClick={() => {
+          props.setInfo(null);
+        }}
+      >
+        Close
+      </p>
+    </InfoBox>
+  );
+};
